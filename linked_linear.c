7739d51727f110ef0d04a54b6df6a2df7c86ebee
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-10-04 07:06:40 +0530

    Update linked_linear.c

--- linked_linear.c
+++ linked_linear.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node{
-	int data;
-	struct node *next;
-}node;
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
 
 node *head = NULL;
 
@@ -15,30 +15,31 @@ void insertEnd(int data);
 void delete(int data);
 void deletell();
 
-int main()
+int main() 
 {
-	int c;
-	
-	do{
-		printf("Enter 1 to insert in linked list\n");
-		printf("Enter 2 to delete from the linked list\n");
-		printf("Enter 3 to traverse the linked list\n");
-		printf("Enter 4 to delete the linked list\n");
-				
-		scanf("%d", &c);
-		
+    int c;
+
+    do 
+    {
+        printf("Enter 1 to insert in linked list\n");
+        printf("Enter 2 to delete from the linked list\n");
+        printf("Enter 3 to traverse the linked list\n");
+        printf("Enter 4 to delete the linked list\n");
+
+        scanf("%d", &c);
+
         int temp;
         int choice;
         int tempref;
-		switch(c)
-		{
-			case 1:
-                printf("1 to insert at beggining, 2 to insert before some node, 3 to insert after some node.\n");
-                printf("Enter 4 for insert at end.\n");
-				printf("Enter the choice: ");
-				scanf("%d", &choice);
-				
-                if (choice == 2 || choice == 3)
+        switch (c) 
+        {
+            case 1:
+                printf("1 to insert at beginning, 2 to insert before some node, 3 to insert after some node.\n");
+                printf("4 to insert at end.\n");
+                printf("Enter the choice: ");
+                scanf("%d", &choice);
+
+                if (choice == 2 || choice == 3) 
                 {
                     printf("Enter the reference node: ");
                     scanf("%d", &tempref);
@@ -47,235 +48,174 @@ int main()
                     scanf("%d", &temp);
 
                     insert_where(temp, tempref, choice);
-                }
-
-                else if (choice == 4)
+                } 
+                else if (choice == 4) 
                 {
                     printf("Enter your data: ");
                     scanf("%d", &temp);
                     insertEnd(temp);
-                }
-
-                else
+                } 
+                else 
                 {
                     printf("Enter your data: ");
                     scanf("%d", &temp);
 
                     insert(temp);
                 }
-                
-				
-				traverse();
-				break;
-				
-			case 2:
+
+                traverse();
+                break;
+
+            case 2:
                 printf("Enter your data to be deleted: ");
                 scanf("%d", &temp);
                 delete(temp);
-				
-				traverse();
-				break;
-				
-			case 3:
-				traverse();
-				break;
-				
-			case 4:
-				break;
-				
-		}
-				
-	}while(c < 4);
-	
-	deletell(head);
-	
-	return 0;
+
+                traverse();
+                break;
+
+            case 3:
+                traverse();
+                break;
+
+            case 4:
+                break;
+        }
+
+    } while (c < 4);
+
+    deletell();
+
+    return 0;
 }
 
-void traverse()
+void traverse() 
 {
-	int count = 0;
-	node *temp = NULL;
-	printf("This is how the list looks like currently\n\n");
-	
-	temp = head;
-	
-	if (temp == NULL)
-	{
-		printf("Linked list empty\n\n");
-		return;
-	}
-	
-	while (temp->next != NULL)
-	{
-		printf("%d --> ", temp->data);
-		temp = temp -> next;
-		count++;
-	}
-    	printf("%d\n\n", temp->data);
-	count++;
-    
-	printf("Number of nodes is: %d\n\n", count);
-	return;
-}
-		 
-void insert(int data)
-{
-	if (head == NULL)
-	{
-		head = (node *)malloc(sizeof(node));
+    int count = 0;
+    node *temp = NULL;
+    printf("This is how the list looks like currently\n\n");
 
-		head->data = data;
-		head->next = NULL;
+    temp = head;
 
-		return;
-	}
-	
-	else
-	{
-		node *temp = (node *)malloc(sizeof(node));
-		temp->next = head;
-		temp->data = data;
-		
-		head = temp;
-		return;
-	}
+    if (temp == NULL) 
+    {
+        printf("Linked list empty\n\n");
+        return;
+    }
+
+    while (temp != NULL) 
+    {
+        printf("%d --> ", temp->data);
+        temp = temp->next;
+        count++;
+    }
+    printf("NULL\n");
+    printf("Number of nodes is: %d\n\n", count);
 }
 
-void insertEnd(int data)
+void insert(int data) 
 {
-    if (head == NULL)
-	{
-		head = (node *)malloc(sizeof(node));
+    node *temp = (node *)malloc(sizeof(node));
+    temp->data = data;
+    temp->next = head;
+    head = temp;
+}
 
-		head->data = data;
-		head->next = NULL;
+void insertEnd(int data) 
+{
+    node *temp = (node *)malloc(sizeof(node));
+    temp->data = data;
+    temp->next = NULL;
 
-		return;
-	}
-	
-	else
-	{
-		node *temp = head;
-        while (temp->next != NULL)
+    if (head == NULL) 
+    {
+        head = temp;
+        return;
+    }
+
+    node *ptr = head;
+    while (ptr->next != NULL) 
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = temp;
+}
+
+void insert_where(int data, int tempref, int choice) 
+{
+    node *temp = head, *store;
+
+    if (choice == 2) 
+    {
+        while (temp->next != NULL && temp->next->data != tempref) 
         {
             temp = temp->next;
         }
+        if (temp->next == NULL) 
+        {
+            printf("Couldn't find the reference.\n\n");
+            return;
+        }
+    } 
+    else 
+    {
+        while (temp != NULL && temp->data != tempref) 
+        {
+            temp = temp->next;
+        }
+        if (temp == NULL) 
+        {
+            printf("Couldn't find the reference.\n\n");
+            return;
+        }
+    }
 
-        node *store = (node *)malloc(sizeof(node));
-		store->data = data;
-		store->next = NULL;
-        temp->next = store;
-
-		return;
-	}
+    store = (node *)malloc(sizeof(node));
+    store->data = data;
+    store->next = temp->next;
+    temp->next = store;
 }
 
-void insert_where(int data, int tempref, int choice)
+void delete(int data) 
 {
-    if (head == NULL)
-	{
-		head = (node *)malloc(sizeof(node));
+    if (head == NULL) 
+    {
+        printf("Linked list is empty, cannot delete.\n\n");
+        return;
+    }
 
-		head->data = data;
-		head->next = NULL;
-
-		return;
-	}
-	
-	else
-	{
+    if (head->data == data) 
+    {
         node *temp = head;
-        node *store;
+        head = head->next;
+        free(temp);
+        return;
+    }
 
-        if (choice == 2)
-        {
-            while (temp->next != NULL)
-            {
-		if (temp->next->data == tempref) break;
-                temp = temp->next;
-            }
+    node *temp = head;
+    while (temp->next != NULL && temp->next->data != data) 
+    {
+        temp = temp->next;
+    }
 
-            if (temp->next->data != tempref && temp->next == NULL)
-            {
-                printf("Couldn't find the reference.\n\n");
-                return;
-            }
-        }
-		
-        else 
-        {
-            while (temp->next != NULL)
-            {
-		if (temp->data == tempref) break;
-                temp = temp->next;
-            }
+    if (temp->next == NULL) 
+    {
+        printf("Element not present in the linked list.\n\n");
+        return;
+    }
 
-            if (temp->data != tempref && temp->next == NULL)
-            {
-                printf("Couldn't find the reference.\n\n");
-                return;
-            }
-        }
-		
-        store = (node *)malloc(sizeof(node));
-        store->data = data;
-        store->next = temp->next;
-        temp->next = store;
-
-		return;
-	}
-}
-		
-void delete(int data)
-{
-	if (head == NULL)
-	{
-		printf("Linked list is empty, cannot delete.\n\n");
-		return;
-	}
-	
-	else
-	{
-		node *temp = head;
-		while (temp->next->data != data && temp->next != NULL)
-		{
-			temp = temp->next;
-		}
-		
-		if (temp->next == NULL)
-		{
-			printf("Element not present in the linked list.\n\n");
-			return;
-		}
-		
-		else if (temp->next->data == data)
-		{
-			node *temp2 = temp->next;
-			temp->next = temp2->next;
-			free(temp2);
-		}
-		return;
-	}
+    node *temp2 = temp->next;
+    temp->next = temp2->next;
+    free(temp2);
 }
 
-void deletell()
+void deletell() 
 {
-	if (head == NULL)
-	{
-		printf("There is no linked list to delete.");
-		return;
-	}
-	
-	else
-	{
-		node *temp = head;
-		while (head != NULL)
-		{
-			head = head->next;
-			free(temp);
-			temp = head;
-		}
-		free(temp);
-	}
+    node *temp = head;
+    while (head != NULL) 
+    {
+        head = head->next;
+        free(temp);
+        temp = head;
+    }
 }
